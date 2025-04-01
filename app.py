052910aeecac2afd09f6e4ee6502a724cb6a4017
James Giannoni <jgiannoni1@gmail.com>
2025-04-01 23:39:38 +0000

    add posgres to reuirements and route to app

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from James Giannoni in 3308'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("https://lab-10-20z0.onrender.com")
+    conn.close()
+    return "Database connection successful"
+
