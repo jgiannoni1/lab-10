d8449a7788c8df3c3ad0aa97bca7102a5389fef5
James Giannoni <jgiannoni1@gmail.com>
2025-04-01 23:51:38 +0000

    create table

--- app.py
+++ app.py
@@ -9,7 +9,23 @@ def hello_world():
 
 @app.route('/db_test')
 def db_test():
-    conn = psycopg2.connect("https://lab-10-20z0.onrender.com")
+    conn = psycopg2.connect("postgresql://lab_10_postgres_8wzi_user:B4SiP5YbCiXEwIth7pR2afsnqOgEqFnD@dpg-cvm7kbngi27c73ad8af0-a/lab_10_postgres_8wzi")
     conn.close()
     return "Database connection successful"
 
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("your_db_url_here")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table created"
